abb684ce934acf64b3b71694bb61e1eaef1300a0
HanBI24 <alsltjdrhs@naver.com>
2022-04-07 23:35:47 +0900

    Add BaseViewMOdel.kt

--- app/src/main/java/com/example/hotplenavigation/view/MainActivity.kt
+++ app/src/main/java/com/example/hotplenavigation/view/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.hotplenavigation.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hotplenavigation.R
+import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
